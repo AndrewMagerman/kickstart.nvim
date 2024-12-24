715d6b1874e3213070fd561513d6cadbbf8a6a86
andrewmagerman <andrew.magerman@magerman.com>
2024-12-24 11:17:18 +0100

    add lsp for python and java

--- init.lua
+++ init.lua
@@ -530,7 +530,8 @@ require('lazy').setup({
       local servers = {
         clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
+        java_language_server = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
